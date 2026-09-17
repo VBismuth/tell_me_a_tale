87aa89b74d6ccf0548ce882bc716c84f5f90a034
VBismuth <work.nicitons@yandex.ru>
2026-09-17 16:07:58 +0300

    Tweaked user input example and increased the version
    [Release]

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,6 @@
 # !!START!!
 TMT_SELF = '''!!SELF!!'''
 TMT_NAME = 'Tell Me a Tale'
-TMT_VERSION = '09.2026.002'
+TMT_VERSION = '09.2026.003'
 TMT_LICENSE = 'GPL 3.0'
 TMT_AUTHOR = 'VBismuth <work.nicitons@yandex.ru>'
